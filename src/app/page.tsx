3cd8f868efa1418b2d923da8f4475a381bc8e782
AbdallahAbdul-Latif <152520316+AbdallahAbdul-Latif@users.noreply.github.com>
2025-07-08 17:42:50 +0200

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import Hero from './components/Hero' // adjust path if needed
-import Header from './components/Header' // adjust path if needed
-import Footer from './components/Footer'
 import Location from './components/Location' // adjust path if needed
 import Team from './components/Team' // adjust path if needed
 
