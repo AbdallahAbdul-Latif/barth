a0738930d1a333e0d33b5c7c70286e8931494fa5
AbdallahAbdul-Latif <152520316+AbdallahAbdul-Latif@users.noreply.github.com>
2025-07-11 23:21:07 +0200

    minor fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ export const metadata = {
   description: 'Rechtsanwaltskanzlei',
 }
 
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="de">
